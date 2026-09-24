b1c08f1554bdc5f9c2342ff4dc21a8b53d38fb40
nirwaneffendy <nirwaneffendy60@gmail.com>
2026-09-24 23:51:57 +0700

    Fix: Make visitor counter 100% authentic starting strictly from 1 with zero artificial numbers

--- counter.php
+++ counter.php
@@ -6,19 +6,21 @@ header('Expires: 0');
 
 $file = 'visitors.txt';
 
+// Pure authentic counter starting strictly from 1
 if (!file_exists($file)) {
-    file_put_contents($file, '1580', LOCK_EX);
+    file_put_contents($file, '1', LOCK_EX);
 }
 
 $current_count = (int)file_get_contents($file);
-if ($current_count < 1000) {
-    $current_count = 1580;
+if ($current_count < 1) {
+    $current_count = 1;
 }
 
 if (session_status() === PHP_SESSION_NONE) {
     @session_start();
 }
 
+// Increment by 1 for each new visitor session
 if (!isset($_SESSION['counted'])) {
     $current_count++;
     file_put_contents($file, (string)$current_count, LOCK_EX);
